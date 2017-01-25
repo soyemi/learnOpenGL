dbcff663407b5c8316005bcae2ec817059ddb82d
Yemi <yemiry@163.com>
2017-01-25 13:59:42 +0800

    basic openGL window

---
+++ OpenGLApp/OpenGLApp/app.cpp
@@ -0,0 +1,48 @@
+#define GLEW_STATIC
+#include <GL\glew.h>
+
+#include <GLFW\glfw3.h>
+#include <iostream>
+
+void key_callback(GLFWwindow * window, int key, int scancode, int action, int mode);
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGLApp", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	glfwSetKeyCallback(window, key_callback);
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		//render command
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		glfwSwapBuffers(window);
+	}
+
+
+	return 0;
+}
+
+void key_callback(GLFWwindow * window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
